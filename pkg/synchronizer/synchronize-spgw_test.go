41bc40f081e8eceb4482571491f9a895ab3296c5
Scott Baker <smbaker@gmail.com>
2020-11-12 15:40:41 -0800

    spgw synchronizer step test

---
+++ pkg/synchronizer/synchronize-spgw_test.go
@@ -0,0 +1,207 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package synchronizer
+
+import (
+	models_v2 "github.com/onosproject/config-models/modelplugin/aether-2.0.0/aether_2_0_0"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+// to facilitate easy declaring of pointers to strings
+func aStr(s string) *string {
+	return &s
+}
+
+// to facilitate easy declaring of pointers to bools
+func aBool(b bool) *bool {
+	return &b
+}
+
+// to facilitate easy declaring of pointers to uint32
+func aUint32(u uint32) *uint32 {
+	return &u
+}
+
+// to facilitate easy declaring of pointers to uint64
+func aUint64(u uint64) *uint64 {
+	return &u
+}
+
+// an empty device should yield empty json
+func TestSynchronizeSpgwEmpty(t *testing.T) {
+	// Get a temporary file name and defer deletion of the file
+	f, err := ioutil.TempFile("", "synchronizer-json")
+	assert.Nil(t, err)
+	tempFileName := f.Name()
+	defer func() {
+		os.Remove(tempFileName)
+	}()
+
+	s := Synchronizer{}
+	s.SetOutputFileName(tempFileName)
+	device := models_v2.Device{}
+	err = s.SynchronizeSpgw(&device)
+	assert.Nil(t, err)
+
+	content, err := ioutil.ReadFile(tempFileName)
+	assert.Nil(t, err)
+	assert.Equal(t, "{}", string(content))
+}
+
+// a fully populated device should yield a decent chunk of json
+func TestSynchronizeSpgwPopulated(t *testing.T) {
+	// Get a temporary file name and defer deletion of the file
+	f, err := ioutil.TempFile("", "synchronizer-json")
+	assert.Nil(t, err)
+	tempFileName := f.Name()
+	defer func() {
+		os.Remove(tempFileName)
+	}()
+
+	s := Synchronizer{}
+	s.SetOutputFileName(tempFileName)
+
+	acp := models_v2.AccessProfile_AccessProfile_AccessProfile{
+		Description: aStr("sample-acp-desc"),
+		DisplayName: aStr("sample-acp-dn"),
+		Filter:      aStr("sample-acp-filter"),
+		Id:          aStr("sample-acp"),
+		Type:        aStr("sample-acp-type"),
+	}
+
+	app := models_v2.ApnProfile_ApnProfile_ApnProfile{
+		ApnName:      aStr("sample-app-name"),
+		Description:  aStr("sample-app-description"),
+		DisplayName:  aStr("sample-app-displayname"),
+		DnsPrimary:   aStr("sample-app-dnsprimary"),
+		DnsSecondary: aStr("sample-app-dnssecondary"),
+		Id:           aStr("sample-app"),
+		GxEnabled:    aBool(true),
+		Mtu:          aUint32(123),
+	}
+
+	qp := models_v2.QosProfile_QosProfile_QosProfile{
+		ApnAmbr:     &models_v2.QosProfile_QosProfile_QosProfile_ApnAmbr{Downlink: aUint32(123), Uplink: aUint32(456)},
+		Description: aStr("sample-qp-desc"),
+		DisplayName: aStr("sample-qp-displayname"),
+		Id:          aStr("sample-qp"),
+	}
+
+	up := models_v2.UpProfile_UpProfile_UpProfile{
+		AccessControl: aStr("sample-up-ac"),
+		Description:   aStr("sample-up-desc"),
+		DisplayName:   aStr("sample-up-displayname"),
+		Id:            aStr("sample-up"),
+		UserPlane:     aStr("sample-up-up"),
+	}
+
+	ue := models_v2.AetherSubscriber_Subscriber_Ue{
+		DisplayName:   aStr("sample-ue-displayname"),
+		Enabled:       aBool(true),
+		Id:            aStr("68a3b12e-253e-11eb-adc1-0242ac120002"),
+		ImsiRangeFrom: aUint64(123456),
+		ImsiRangeTo:   aUint64(1234567),
+		Priority:      aUint32(1),
+		Profiles: &models_v2.AetherSubscriber_Subscriber_Ue_Profiles{
+			ApnProfile: aStr("sample-app"),
+			QosProfile: aStr("sample-qp"),
+			UpProfile:  aStr("sample-up"),
+			AccessProfile: map[string]*models_v2.AetherSubscriber_Subscriber_Ue_Profiles_AccessProfile{
+				"sample-acp": &models_v2.AetherSubscriber_Subscriber_Ue_Profiles_AccessProfile{
+					AccessProfile: aStr("sample-acp"),
+					Allowed:       aBool(true),
+				},
+			},
+		},
+		RequestedApn: aStr("sample-ue-req-apn"),
+		ServingPlmn: &models_v2.AetherSubscriber_Subscriber_Ue_ServingPlmn{
+			Mcc: aUint32(123),
+			Mnc: aUint32(456),
+			Tac: aUint32(789),
+		},
+	}
+
+	device := models_v2.Device{
+		AccessProfile: &models_v2.AccessProfile_AccessProfile{AccessProfile: map[string]*models_v2.AccessProfile_AccessProfile_AccessProfile{"sample-acp": &acp}},
+		ApnProfile:    &models_v2.ApnProfile_ApnProfile{ApnProfile: map[string]*models_v2.ApnProfile_ApnProfile_ApnProfile{"sample-app": &app}},
+		QosProfile:    &models_v2.QosProfile_QosProfile{QosProfile: map[string]*models_v2.QosProfile_QosProfile_QosProfile{"sample-qp": &qp}},
+		Subscriber:    &models_v2.AetherSubscriber_Subscriber{Ue: map[string]*models_v2.AetherSubscriber_Subscriber_Ue{"68a3b12e-253e-11eb-adc1-0242ac120002": &ue}},
+		UpProfile:     &models_v2.UpProfile_UpProfile{UpProfile: map[string]*models_v2.UpProfile_UpProfile_UpProfile{"sample-up": &up}},
+	}
+
+	err = s.SynchronizeSpgw(&device)
+	assert.Nil(t, err)
+
+	// define the expected json here
+	expected_result := `{
+  "subscriber-selection-rules": [
+    {
+      "priority": 1,
+      "keys": {
+        "imsi-range": {
+          "from": 123456,
+          "to": 1234567
+        },
+        "serving-plmn": {
+          "mcc": 123,
+          "mnc": 456,
+          "tac": 789
+        },
+        "requested-apn": "sample-ue-req-apn"
+      },
+      "selected-apn-profile": "sample-app",
+      "selected-access-profile": [
+        "sample-acp"
+      ],
+      "selected-qos-profile": "sample-qp",
+      "selected-user-plane-profile": "sample-up"
+    }
+  ],
+  "access-profiles": {
+    "sample-acp": {
+      "type": "sample-acp-type",
+      "filter": "sample-acp-filter"
+    }
+  },
+  "apn-profiles": {
+    "sample-app": {
+      "apn-name": "sample-app-name",
+      "dns-primary": "sample-app-dnsprimary",
+      "dns-secondary": "sample-app-dnssecondary",
+      "mtu": 123,
+      "gx-enabled": true,
+      "network": "lbo",
+      "usage": 1
+    }
+  },
+  "qos-profiles": {
+    "sample-qp": {
+      "apn-ambr": [
+        123,
+        456
+      ]
+    }
+  },
+  "user-plane-profiles": {
+    "sample-up": {
+      "user-plane": "sample-up-up",
+      "access-control": "sample-up-ac",
+      "access-tags": {
+        "tag1": "ACC"
+      },
+      "qos-tags": {
+        "tag1": "BW"
+      }
+    }
+  }
+}`
+
+	content, err := ioutil.ReadFile(tempFileName)
+	assert.Nil(t, err)
+	assert.Equal(t, expected_result, string(content))
+}

e2cbd84ab8e68ffd8f97f2f3530646fa6f94bf2b
Scott Baker <smbaker@gmail.com>
2021-01-06 10:29:35 -0800

    check in missing file

---
+++ pkg/synchronizer/util.go
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+// Utility functions for synchronizer
+package synchronizer
+
+func boolToUint32(b bool) uint32 {
+	if b {
+		return 1
+	} else {
+		return 0
+	}
+}

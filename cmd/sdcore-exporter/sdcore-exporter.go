8799fa4e57f0e18aebfcea7e61c9756aff150741
Sean Condon <sean@opennetworking.org>
2021-07-14 14:00:44 +0100

    exporter: adding stats for all UEs

--- cmd/sdcore-exporter/sdcore-exporter.go
+++ cmd/sdcore-exporter/sdcore-exporter.go
@@ -15,9 +15,9 @@ import (
 
 func main() {
 
-	collector.RecordMetrics(2*time.Second, "starbucks_newyork_cameras")
-	collector.RecordMetrics(2*time.Second, "starbucks_seattle_cameras")
-	collector.RecordMetrics(2*time.Second, "acme_chicago_robots")
+	collector.RecordMetrics(2*time.Second, "starbucks-newyork-cameras")
+	collector.RecordMetrics(2*time.Second, "starbucks-seattle-cameras")
+	collector.RecordMetrics(2*time.Second, "acme-chicago-robots")
 
 	// period, vcs, imsis, upMbps, downMbps, upLatency, downLatency
 	collector.RecordUEMetrics(2*time.Second, "starbucks-newyork-cameras", []string{
@@ -29,6 +29,48 @@ func main() {
 		"170029313275053",
 		"170029313275054",
 		"170029313275055",
+	}, 100000, 1000, 10, 10)
+
+	collector.RecordUEMetrics(2*time.Second, "starbucks-seattle-cameras", []string{
+		"170029313275000",
+		"170029313275001",
+		"170029313275002",
+		"170029313275003",
+		"170029313275010",
+		"170029313275011",
+		"170029313275012",
+		"170029313275013",
+		"170029313275014",
+	}, 100000, 1000, 10, 10)
+
+	collector.RecordUEMetrics(2*time.Second, "starbucks-seattle-pos", []string{
+		"170029313275020",
+		"170029313275021",
+		"170029313275022",
+		"170029313275030",
+		"170029313275031",
+		"170029313275032",
+		"170029313275033",
+		"170029313275034",
+	}, 100000, 1000, 10, 10)
+
+	collector.RecordUEMetrics(2*time.Second, "starbucks-newyork-pos", []string{
+		"170029313275060",
+		"170029313275061",
+		"170029313275070",
+		"170029313275071",
+		"170029313275072",
+		"170029313275073",
+	}, 100000, 1000, 10, 10)
+
+	collector.RecordUEMetrics(2*time.Second, "acme-chicago-robots", []string{
+		"13698808332993000",
+		"13698808332993001",
+		"13698808332993002",
+		"13698808332993003",
+		"13698808332993010",
+		"13698808332993011",
+		"13698808332993012",
 	}, 100000, 1000, 10, 10)
 
 	var imsi uint64

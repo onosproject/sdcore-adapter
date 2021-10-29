a3d62d41961647afce209449993e25c4c4f51063
sbconsulting <sbconsulting@users.noreply.github.com>
2021-10-29 01:02:49 -0700

    AETHER-2554: Plumb through burst sizes (#109)
    Co-authored-by: Scott Baker <smbaker@gmail.com>

--- pkg/synchronizer/v4/synchronize-upf_test.go
+++ pkg/synchronizer/v4/synchronize-upf_test.go
@@ -44,6 +44,60 @@ func TestSynchronizeVcsUPF(t *testing.T) {
 			"sliceQos": {
 				"uplinkMBR": 333,
 				"downlinkMBR": 444,
+				"uplinkBurstSize": 625000,
+				"downlinkBurstSize": 625000,
+				"bitrateUnit": "bps"
+			},
+			"ueResourceInfo": [
+				{
+					"uePoolId": "sample-dg",
+					"dnn": "5ginternet"
+				}
+			]
+		}`
+
+		require.JSONEq(t, expectedResult, json)
+	}
+}
+
+func TestSynchronizeVcsUPFWithBurst(t *testing.T) {
+	m := NewMemPusher()
+	s := Synchronizer{}
+	s.SetPusher(m)
+
+	ent, cs, tcList, ipd, site, dg := BuildSampleDeviceGroup()
+	apps, tp, upf, vcs := BuildSampleVcs()
+
+	vcs.Slice.Mbr.UplinkBurstSize = aUint32(111222)
+	vcs.Slice.Mbr.DownlinkBurstSize = aUint32(333444)
+
+	device := models.Device{
+		Enterprise:          &models.OnfEnterprise_Enterprise{Enterprise: map[string]*models.OnfEnterprise_Enterprise_Enterprise{"sample-ent": ent}},
+		ConnectivityService: &models.OnfConnectivityService_ConnectivityService{ConnectivityService: map[string]*models.OnfConnectivityService_ConnectivityService_ConnectivityService{"sample-cs": cs}},
+		Site:                &models.OnfSite_Site{Site: map[string]*models.OnfSite_Site_Site{"sample-site": site}},
+		IpDomain:            &models.OnfIpDomain_IpDomain{IpDomain: map[string]*models.OnfIpDomain_IpDomain_IpDomain{"sample-ipd": ipd}},
+		DeviceGroup:         &models.OnfDeviceGroup_DeviceGroup{DeviceGroup: map[string]*models.OnfDeviceGroup_DeviceGroup_DeviceGroup{*dg.Id: dg}},
+		Application:         &models.OnfApplication_Application{Application: apps},
+		Template:            &models.OnfTemplate_Template{Template: map[string]*models.OnfTemplate_Template_Template{*tp.Id: tp}},
+		TrafficClass:        &models.OnfTrafficClass_TrafficClass{TrafficClass: tcList},
+		Upf:                 &models.OnfUpf_Upf{Upf: map[string]*models.OnfUpf_Upf_Upf{*upf.Id: upf}},
+		Vcs:                 &models.OnfVcs_Vcs{Vcs: map[string]*models.OnfVcs_Vcs_Vcs{*vcs.Id: vcs}},
+	}
+
+	pushFailures, err := s.SynchronizeVcsUPF(&device, vcs)
+	assert.Nil(t, err)
+	t.Logf("%+v", m.Pushes)
+	json, okay := m.Pushes["http://upf/v1/config/network-slices"]
+	assert.True(t, okay)
+	assert.Equal(t, 0, pushFailures)
+	if okay {
+		expectedResult := `{
+			"sliceName": "sample-vcs",
+			"sliceQos": {
+				"uplinkMBR": 333,
+				"downlinkMBR": 444,
+				"uplinkBurstSize": 111222,
+				"downlinkBurstSize": 333444,
 				"bitrateUnit": "bps"
 			},
 			"ueResourceInfo": [

23fe580eb47d3d557e09ac971d82f70698ff9e41
Scott Baker <smbaker@gmail.com>
2021-12-02 14:59:41 -0800

    Aether-2322 Be more rigorous about enums

---
+++ pkg/gnmi/defs_test.go
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+package gnmi
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// Ensure that TestConfigCallbackType.String() does not fail for all valid enum values
+func TestConfigCallbackType(t *testing.T) {
+	for ct := ConfigCallbackType(0); ct < configCallbackTypeLimit; ct++ {
+		assert.NotEmpty(t, ct.String())
+	}
+}
